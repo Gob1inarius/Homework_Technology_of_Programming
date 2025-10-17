e06e616dc2da2242dfd88ad4ecae1dabe1d5628a
Gob1inarius <Vladosplav@yandex.ru>
2025-10-18 02:38:40 +0300

    Добавил ввод двух чисел

--- TechPro.c
+++ TechPro.c
@@ -2,5 +2,10 @@
 
 int main(){
 printf("Hello, World!");
+int num1, num2;
+printf("Enter the first number: ");
+scanf("%d", &num1);
+printf("Enter the second number: ");
+scanf("%d", &num2);
     return 0;
 }
